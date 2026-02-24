51f4f582be1b6f35032869105b751835cce3e95e
OyakXD <kayovieira157@gmail.com>
2026-02-24 10:51:56 -0300

    Chamando o código principal para o fluxograma

---
+++ main.py
@@ -0,0 +1,31 @@
+from movimentacao import *
+from plantacao import *
+
+# MOVIMENTAÇÃO
+while True:
+	
+	plantar_tree()
+	move_direita(5)
+	move_cima(1)
+	plantar_tree()
+	move_esquerda(5)
+	move_cima(1)
+	plantar_tree()
+	move_direita(5)
+	move_cima(1)
+	plantar_tree()
+	move_esquerda(5)
+	move_cima(1)
+	plantar_tree()
+	
+	
+	
+			
+		
+
+
+
+
+
+
+	
