5b014a5706a863b806b5bcf96285688e45c1a71d
OyakXD <kayovieira157@gmail.com>
2026-02-26 19:22:20 -0300

    Percebi que fiz funções quase iguais, então apliquei uma lógica para juntar-lás

--- movimentacao.py
+++ movimentacao.py
@@ -8,29 +8,19 @@ def mover(n, dir, agir=True):
 def ir(n, dir):
 	mover(n, dir, False)
 	
-
-def preparar_campo():
-	for linha in range(16):
-		for coluna in range(16):
-			till()
-			if coluna < 15:
-				if linha % 2 == 0:
-					ir(1, East)
-				else:
-					ir(1, West)
-		if linha < 15:
-			ir(1, North)
-
-def percorrer_campo():
-	for linha in range(16):
-		for coluna in range(16):
-			harvest()
-			plant(Entities.Pumpkin)
-			use_item(Items.Water)
-			if coluna < 15:
-				if linha % 2 == 0:
-					ir(1, East)
-				else:
-					ir(1, West)
-		if linha < 15:
-			ir(1, North)
+def percorrer_campo(preparando=False):
+    for linha in range(16):
+        for coluna in range(16):
+            if preparando:
+                till()
+            else:
+                harvest()
+                plant(Entities.Pumpkin)
+                use_item(Items.Water)
+            if coluna < 15:
+                if linha % 2 == 0:
+                    ir(1, East)
+                else:
+                    ir(1, West)
+        if linha < 15:
+            ir(1, North)   
